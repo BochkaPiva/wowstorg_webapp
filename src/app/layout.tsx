f0f0581442f7f74926693ed8ac2cdad6c71b9cb9
BochkaPiva <BochkaPiva@users.noreply.github.com>
2026-08-04 16:39:20 +0600

    Обновить favicon сайта

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon-16.png" sizes="16x16" type="image/png" />
+        <link rel="icon" href="/favicon-32.png" sizes="32x32" type="image/png" />
+        <link rel="icon" href="/favicon-192.png" sizes="192x192" type="image/png" />
+        <link rel="icon" href="/favicon-512.png" sizes="512x512" type="image/png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" sizes="180x180" />
+      </head>
       <body>
         <Providers>{children}</Providers>
       </body>
